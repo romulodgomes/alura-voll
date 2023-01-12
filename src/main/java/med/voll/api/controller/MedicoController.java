a1df25b8d7215aba276a4ccdfa5259c1ef472f1d
Rômulo Gomes <romulo.redg@gmail.com>
2023-01-12 13:25:29 -0300

    Inserção de medico no db e validacoes

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,14 +1,24 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.dto.DadosCadastroMedico;
+import med.voll.api.model.Medico;
+import med.voll.api.repositories.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
     @PostMapping()
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
+
+        repository.save(new Medico(dados));
     }
 
 }
